0d72c21e5223e041e538e1f7ee9e13e1c9dd30db
Alexander Vielimchanitsia <alexandervielimchanitsia@gmail.com>
2019-03-14 00:15:06 +0200

    add lives to the dashboard

--- src/js/layers/dashboard.ts
+++ src/js/layers/dashboard.ts
@@ -5,20 +5,24 @@ export function createDashboardLayer(font: Font, playerEnv: PlayerEnv) {
   const LINE1 = font.size;
   const LINE2 = font.size * 2;
 
-  const coins = 123;
+  const coins = 0;
+  const lives = 3;
 
   return function drawDashboard(context: CanvasRenderingContext2D) {
     const {score, time} = playerEnv.playerController;
 
-    font.print('MARIO', context, 16, LINE1);
-    font.print(score.toString().padStart(6, '0'), context, 16, LINE2);
+    font.print('MARIO', context, 8, LINE1);
+    font.print(score.toString().padStart(6, '0'), context, 8, LINE2);
 
-    font.print('@x' + coins.toString().padStart(2, '0'), context, 96, LINE2);
+    font.print('@x' + coins.toString().padStart(2, '0'), context, 67, LINE2);
 
-    font.print('WORLD', context, 152, LINE1);
-    font.print('1-1', context, 160, LINE2);
+    font.print('WORLD', context, 112, LINE1);
+    font.print('1-1', context, 120, LINE2);
 
-    font.print('TIME', context, 208, LINE1);
-    font.print(time.toFixed().toString().padStart(3, '0'), context, 216, LINE2);
+    font.print('TIME', context, 164, LINE1);
+    font.print(time.toFixed().toString().padStart(3, '0'), context, 172, LINE2);
+
+    font.print('LIVES', context, 208, LINE1);
+    font.print(lives.toString(), context, 224, LINE2);
   };
 }
